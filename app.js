0b9c867a5eb4eb3be2067e444ea3ea605ee70a36
LeonardoBujan <tatobujan86@gmail.com>
2021-10-19 00:20:05 -0300

    servicio creados: berners, clarke, hamilton

--- app.js
+++ app.js
@@ -19,4 +19,20 @@ app.get("/", (req, res)=>{
 app.get("/babbage", (req, res)=>{
     const rutaBabbage = path.join(__dirname, "./views/babbage.html");
     res.sendFile(rutaBabbage);
+});
+
+app.get("/berners-lee", (req, res)=>{
+    const rutaBerners = path.join(__dirname, "./views/berners-lee.html");
+    res.sendFile(rutaBerners);
+});
+
+app.get("/clarke", (req, res)=>{
+    const rutaClarke = path.join(__dirname, "./views/clarke.html");
+    res.sendFile(rutaClarke);
+});
+
+app.get("/hamilton", (req, res)=>{
+    const rutaHamilton = path.join(__dirname, "./views/hamilton.html");
+    res.sendFile(rutaHamilton);
+
 })
